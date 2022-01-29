522662342eec424e29e6b82b65dfbc3dbf1339c2
suhaswani <54031042+suhaswani@users.noreply.github.com>
2022-01-29 21:31:43 +0530

    Added Models

--- git_pract/a1/models.py
+++ git_pract/a1/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class student(models.Model):
+    pass
+
+
+class Teacher(models.Model):
+    pass
+
+
+class College(models.Model):
+    pass
+
