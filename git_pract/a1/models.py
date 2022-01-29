e59c47ad6a2a79cecd60dc8915dd8fd30b5a6386
suhaswani <54031042+suhaswani@users.noreply.github.com>
2022-01-30 01:27:53 +0530

    Added fields in models

--- git_pract/a1/models.py
+++ git_pract/a1/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 
 class student(models.Model):
-    pass
+    s_name = models.CharField(max_length=50)
 
 
 class Teacher(models.Model):
-    pass
+    t_name = models.CharField(max_length=50)
 
 
 class College(models.Model):
-    pass
+    cl_name = models.CharField(max_length=50)
 
